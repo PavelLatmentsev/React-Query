81dfe3ad7362428e566fd35dec66e468fc998f33
Pavel <nightcharm@mail.ru>
2023-09-05 21:07:50 +0300

    add useQury, add Provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
-import "./App.css";
-
+import { useQuery } from "@tanstack/react-query";
+const todoId=1;
 function App() {
-  return <h1>Hello React-Query</h1>;
+  const {isLoading, isSuccess,error, data}=useQuery(["todos", todoId],()=>fetch("https://jsonplaceholder.typicode.com/todos/1").then(res=>res.json()));
+  console.log(data, isLoading, isSuccess,error,)
+  return (<>
+  {data ? <h1>Todo: {data.title}</h1>: <h1>Идет загрузка</h1>}
+  </>);
 }
 
 export default App;
