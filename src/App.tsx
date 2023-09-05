819c50cfe8e31aae80eea984628b71425d693077
Pavel <nightcharm@mail.ru>
2023-09-06 00:29:39 +0300

    add axios and create loading

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,29 @@
 import { useQuery } from "@tanstack/react-query";
-const todoId=1;
+import axios from "axios";
+import { ITodo } from "./app.interface";
+const todoId = 1;
 function App() {
-  const {isLoading, isSuccess,error, data}=useQuery(["todos", todoId],()=>fetch("https://jsonplaceholder.typicode.com/todos/1").then(res=>res.json()));
-  console.log(data, isLoading, isSuccess,error,)
-  return (<>
-  {data ? <h1>Todo: {data.title}</h1>: <h1>Идет загрузка</h1>}
-  </>);
+  // const {isLoading, isSuccess,error, data}=useQuery(["todos", todoId],()=>fetch("https://jsonplaceholder.typicode.com/todos/1").then(res=>res.json())); // вариант с фетчем
+  const { isLoading, isSuccess, error, data } = useQuery(
+    ["todos", todoId],
+    () => axios.get<ITodo>("https://jsonplaceholder.typicode.com/todos/1"),
+    {
+      select: ({ data }) => data,
+    }
+  );
+  console.log(data, isLoading, isSuccess, error);
+  return (
+    <div>
+      {/* {error && <div>{error}</div>} */}
+      {isLoading ? (
+        <div>Loading...</div>
+      ) : data ? (
+        <h1>Todo: {data.title}</h1>
+      ) : (
+        <h1>data not found</h1>
+      )}
+    </div>
+  );
 }
 
 export default App;
